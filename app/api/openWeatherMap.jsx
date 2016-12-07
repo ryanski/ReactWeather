bfa912c53d6fef8db1ca62ae390536d8ef629992
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2016-12-06 18:01:02 -0600

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=2fe1d0b5ee9c00ed540de5a3e22da01a&units=imperial';
+
+
+module.exports = {
+	getTemp: function (location) {
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+		return axios.get(requestUrl).then(function (res) {
+			if (res.data.cod && res.data.message) {
+				throw new Error(res.data.message);
+			} else {
+				return res.data.main.temp;
+			}
+		}, function(err) {
+  			throw new Error(err.response.data.message);
+		});
+	}
+}	
