dcaed8a3baef4cc3091f50639cf087d07b53c54f
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2017-01-04 19:49:36 -0600

    Added  nav search support and cleaned up code

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,23 +1,17 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-// var Nav = React.createClass({
-// 	render: function () {
-// 		return (
-// 			<div>
-// 				<h2>Nav Component</h2>
-// 				<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-// 				<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-// 				<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-// 			</div>
-// 			);
-// 	}
-// });
-
 var Nav = React.createClass({
 	onSearch: function (e) {
 		e.preventDefault();
-		alert('Not yet wired up!');
+		
+		var location = this.refs.search.value;
+		var encodedLocation = encodeURIComponent(location);
+
+		if (location.length > 0) {
+			this.refs.search.value = '';
+			window.location.hash = '#/?location=' + encodedLocation;
+		}
 	},
 	render: function () {
 		return (
@@ -40,7 +34,7 @@ var Nav = React.createClass({
 				<form onSubmit={this.onSearch}>
 					<ul className="menu">
 						<li>
-							<input type="search" placeholder="Search weather by city"/>
+							<input type="search" placeholder="Search weather by city" ref="search"/>
 						</li>
 						<li>
 							<input type="submit" className="button" value="Get Weather"/>
